345702e990295cf6076c743655794a2029e262e8
aysesimsek <37701256+aysesimsek@users.noreply.github.com>
2019-10-18 16:08:24 +0300

    background

---
+++ Assets/Scripts/BackgroundMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform BackgroundCenter;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y >= BackgroundCenter.position.y + 10f)
+        {
+            BackgroundCenter.position = new Vector2(BackgroundCenter.position.x, transform.position.y + 10f);
+        }
+        else if (transform.position.y <= BackgroundCenter.position.y - 10f)
+        {
+            BackgroundCenter.position = new Vector2(BackgroundCenter.position.x, transform.position.y - 10F);
+        }
+    }
+}
