cb14fd687c91e76f138308808ef2cde9b6e42457
aysesimsek <37701256+aysesimsek@users.noreply.github.com>
2019-10-21 15:11:27 +0300

    new meteor enemies are added.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -55,7 +55,16 @@ public class PlayerControl : MonoBehaviour {
     public GameObject enemy_38;
     public GameObject enemy_39;
     public GameObject enemy_40;
-
+    public GameObject enemy_41;
+    public GameObject enemy_42;
+    public GameObject enemy_43;
+    public GameObject enemy_44;
+    public GameObject enemy_45;
+    public GameObject enemy_46;
+    public GameObject enemy_47;
+    public GameObject enemy_48;
+    public GameObject enemy_49;
+    public GameObject enemy_50;
     GameObject[] enemyCollection;
 
     float next_time;
@@ -67,7 +76,7 @@ public class PlayerControl : MonoBehaviour {
         InvokeRepeating("Scorer", 0, 1);
         next_time = Time.time + 0.5f;
 
-     enemyCollection = new GameObject[] { enemy_1, enemy_2, enemy_3, enemy_4, enemy_5, enemy_6, enemy_7, enemy_8, enemy_9, enemy_10, enemy_11, enemy_12, enemy_13, enemy_14, enemy_15, enemy_16, enemy_17, enemy_18, enemy_19, enemy_20, enemy_21, enemy_22, enemy_23, enemy_24, enemy_25, enemy_26, enemy_27, enemy_28, enemy_29, enemy_30, enemy_31, enemy_32, enemy_33, enemy_34, enemy_35, enemy_36, enemy_37, enemy_38, enemy_39, enemy_40 };
+     enemyCollection = new GameObject[] { enemy_1, enemy_2, enemy_3, enemy_4, enemy_5, enemy_6, enemy_7, enemy_8, enemy_9, enemy_10, enemy_11, enemy_12, enemy_13, enemy_14, enemy_15, enemy_16, enemy_17, enemy_18, enemy_19, enemy_20, enemy_21, enemy_22, enemy_23, enemy_24, enemy_25, enemy_26, enemy_27, enemy_28, enemy_29, enemy_30, enemy_31, enemy_32, enemy_33, enemy_34, enemy_35, enemy_36, enemy_37, enemy_38, enemy_39, enemy_40, enemy_41, enemy_42, enemy_43, enemy_44, enemy_45, enemy_46, enemy_47, enemy_48, enemy_49, enemy_50 };
 
     //enemyCollection[0]=enemy_1;
 
@@ -126,13 +135,13 @@ public class PlayerControl : MonoBehaviour {
         float random_x = Random.Range(-7.0F, 7.0F);
         float random_y = Random.Range(3.0F, 7.5F);
 
-        int enemy_no = Random.Range(0,30);
+        int enemy_no = Random.Range(0,50);
 
         GameObject currentEnemy = enemyCollection[enemy_no];
 
         GameObject new_enemy = Instantiate(currentEnemy, new Vector3(transform.position.x + random_x, transform.position.y + random_y, transform.position.z), Quaternion.identity);
 
-        Destroy(new_enemy, 30);
+        Destroy(new_enemy, 50);
     }
 
 }
