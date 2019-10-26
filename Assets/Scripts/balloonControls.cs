39c0ddb94df52f3d3e1aa00e9168f820e091e31c
gozdeyildirmaz <gozdeyildirmaz@gmail.com>
2019-10-26 15:22:22 +0300

    ms
    reklamı eklendiii

--- Assets/Scripts/balloonControls.cs
+++ Assets/Scripts/balloonControls.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GoogleMobileAds.Api;
+using System;
 
 public class balloonControls : MonoBehaviour
 {
     GameObject player;
+    private InterstitialAd interstitial;
 
     void Start()
     {
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(initStatus => { });
+        RequestInterstitial();
         player = GameObject.Find("Player");
     }
     void Update()
@@ -23,11 +29,76 @@ public class balloonControls : MonoBehaviour
             player.GetComponent<PlayerControl>().enabled = false;
             SceneManager.LoadScene("GameOverMenu");
             Debug.Log("Game Over!");
+            if (this.interstitial.IsLoaded())
+            {
+                this.interstitial.Show();
+            }
         }
         else if (collision.tag == "LevelEnd")
         {
             collision.tag = "Untagged";
         }
     }
-    
+
+    private void RequestInterstitial()
+{
+    #if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+    #elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+    #else
+        string adUnitId = "unexpected_platform";
+    #endif
+
+    // Initialize an InterstitialAd.
+    this.interstitial = new InterstitialAd(adUnitId);
+
+        // Called when an ad request has successfully loaded.
+        this.interstitial.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.interstitial.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is shown.
+        this.interstitial.OnAdOpening += HandleOnAdOpened;
+        // Called when the ad is closed.
+        this.interstitial.OnAdClosed += HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        this.interstitial.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+
+        print("Interstitial failed to load: " + args.Message);
+
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+        interstitial.Destroy();
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeavingApplication event received");
+    }
+
 }
